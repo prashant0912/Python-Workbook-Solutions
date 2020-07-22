a7f34c37974ddb953749d747e902aa6c74a22cc5
Prashant Maurya <65214761+prashant0912@users.noreply.github.com>
2020-07-22 15:50:04 +0530

    Add files via upload

---
+++ vowel or consonant.py
@@ -0,0 +1,8 @@
+a = input("Enter an alphabet:")
+if (a=="A"or a=="a"or a=="E"or a=="e" or a =="i"or a=="I" or a=="o" or a=="O" or a=="u" or
+    a=="U"):
+    print("Vowel")
+else:
+    print("consonant")
+
+
